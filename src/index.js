19385a9c4ebcbc2a5b013e4705d4e033917bb15e
Romanoli <inthewavves@gmail.com>
2020-09-30 16:01:50 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0 || array === null || array === undefined) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0 || array === null || array === undefined) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0 || array === null || array === undefined) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach((item) => {
+    sum += item;
+  });
+  return sum / array.length;
 }
